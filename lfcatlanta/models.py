8467446a1177aa06736e9b1e5b77e6cffca74580
Brandon Ivey <teenageriot@gmail.com>
2015-01-16 18:47:10 -0500

    stupid user model add stuff

--- lfcatlanta/models.py
+++ lfcatlanta/models.py
@@ -4,13 +4,13 @@ from django.utils.translation import ugettext, ugettext_lazy as _
 
 class LFCSiteProfile(models.Model):
     user = models.OneToOneField("auth.User")
-    date_joined = models.DateField(blank=True, null=True)
     lfc_membership_id = models.CharField(_("LFC Membership ID"), max_length=64, blank=True, default='')
     phone = models.CharField(max_length=50, blank=True)
     bio = models.TextField(blank=True, default='')
     shirt = models.BooleanField(default=False)
     card = models.BooleanField(default=False)
     paid = models.CharField(max_length=6, blank=True)
+    lfcatlanta_id = models.CharField(max_length=4, blank=True, null=True)
 
     class Meta:
         verbose_name = "LFC Site Profile"

ed03704f21916a67d692860baf5d28f22bb18357
Brandon Ivey <teenageriot@gmail.com>
2015-01-16 17:55:28 -0500

    adding fields to lfc user

--- lfcatlanta/models.py
+++ lfcatlanta/models.py
@@ -4,10 +4,13 @@ from django.utils.translation import ugettext, ugettext_lazy as _
 
 class LFCSiteProfile(models.Model):
     user = models.OneToOneField("auth.User")
-    date_of_birth = models.DateField(blank=True, null=True)
+    date_joined = models.DateField(blank=True, null=True)
     lfc_membership_id = models.CharField(_("LFC Membership ID"), max_length=64, blank=True, default='')
     phone = models.CharField(max_length=50, blank=True)
     bio = models.TextField(blank=True, default='')
+    shirt = models.BooleanField(default=False)
+    card = models.BooleanField(default=False)
+    paid = models.CharField(max_length=6, blank=True)
 
     class Meta:
         verbose_name = "LFC Site Profile"

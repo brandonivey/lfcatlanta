3e0164ea1d4568a90b55fe86817e6ea51162c04d
Brandon Ivey <teenageriot@gmail.com>
2015-01-16 21:51:43 -0500

    adding admin settings

--- lfcatlanta/admin.py
+++ lfcatlanta/admin.py
@@ -4,6 +4,6 @@ from .models import LFCSiteProfile
 
 
 class LFCSiteProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'last_name', 'first_name', 'lfc_membership_id', 'email')
+    list_display = ('user', 'last_name', 'first_name', 'lfc_membership_id', 'email', 'paid', 'card', 'shirt', 'lfcatlanta_id')
 
 admin.site.register(LFCSiteProfile, LFCSiteProfileAdmin)
